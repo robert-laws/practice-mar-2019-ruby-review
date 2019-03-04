46e25de31a6bfb76336ad8ffe23d86e0113526b0
Robert Laws <roblaws@me.com>
2019-03-04 10:30:19 -0500

    begin string class methods

---
+++ string-class.rb
@@ -0,0 +1,19 @@
+# string class
+# there are several built-in methods that can be used as part of the string class
+
+word = "greetings People"
+
+word.upcase
+# GREETING PEOPLE
+
+word.downcase
+# greeting people
+
+word.capitalize
+# Greeting people
+
+word.reserve
+# elpoeP gniteerg
+
+word.swapcase
+# GREETING pEOPLE
