f8cde7a614e32a6348c43fb99ea8937805237024
Robert Laws <roblaws@me.com>
2019-03-04 09:28:00 -0500

    update control flow

--- control-flow.rb
+++ control-flow.rb
@@ -1,1 +1,26 @@
 # control flow
+# handled by operators if, else, and elsif in a ruby program
+
+name = "bob"
+age = 23
+
+# if statement
+if name == "bob"
+  puts "it's bob"
+end
+
+# else keyword
+if name == "hal"
+  puts "it's hal"
+else
+  puts "it's someone else"
+end
+
+# elsif statement
+if name == "kal"
+  puts "it's kal"
+elsif name == "mel"
+  puts "it's mel"
+else
+  puts "it's someone else"
+end
