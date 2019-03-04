21c51a574ae2eced810007b114d547e4a8a63c5b
Robert Laws <roblaws@me.com>
2019-03-04 10:56:36 -0500

    add control statements to control-flow

--- control-flow.rb
+++ control-flow.rb
@@ -24,3 +24,14 @@ elsif name == "mel"
 else
   puts "it's someone else"
 end
+
+# ternary operator is a shorthand way to write an if/else statement
+location = "boston"
+price = 4.75
+
+location == "new york" ? "we're in NYC" : "Not NYC"
+price > 4 ? "expensive price" : "cheap price"
+
+# statement modifiers - if and unless can be used to quickly test a condition and return a result
+puts "price is expensive" if price > 5
+puts "this price is not expensive" unless price > 5
